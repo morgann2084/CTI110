c0f3ac19e0d77e18799c0e07a290e89099a119d8
morgann2084 <55396782+morgann2084@users.noreply.github.com>
2019-09-16 10:07:29 -0400

    Add files via upload
    First submission.

---
+++ P2HW2_TurtleGraphic_NicolasMorgan.py
@@ -0,0 +1,30 @@
+# This program will use the turtle graphic to draw shapes on a canvas.
+# 9/16/2019
+# CTI-110 P2HW2
+# Nicolas Morgan
+
+import turtle
+
+turtle.speed(1)
+
+turtle.left(45)
+turtle.forward(60)
+turtle.left(90)
+turtle.forward(60)
+turtle.left(90)
+turtle.forward(60)
+turtle.left(90)
+turtle.forward(60)
+turtle.penup()
+turtle.left(90)
+turtle.forward(60)
+turtle.pendown()
+turtle.forward(60)
+turtle.right(90)
+turtle.forward(60)
+turtle.right(90)
+turtle.forward(60)
+turtle.right(90)
+turtle.forward(60)
+turtle.hideturtle()
+turtle.done()
