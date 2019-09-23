c6f7b19bc7a3650833a1994a1c3ca631ae069493
morgann2084 <55396782+morgann2084@users.noreply.github.com>
2019-09-23 10:30:51 -0400

    Add files via upload

---
+++ CW_ifElse_Salary_SHORT.py
@@ -0,0 +1,12 @@
+
+salary = float(input("Enter salary "))
+
+years_on_job = float(input("Enter years worked "))
+
+if salary >= 30000 and years_on_job >= 2:
+    print("You qualify for a loan")
+
+else:
+    print("You don't qualify for a loan")
+
+
