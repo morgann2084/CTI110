fda6639f8fa6a6a6f7d73881961a596289d7606c
morgann2084 <55396782+morgann2084@users.noreply.github.com>
2019-09-23 10:24:52 -0400

    Add files via upload

---
+++ CWifElseSalary.py
@@ -0,0 +1,18 @@
+
+salary = float(input("Enter salary "))
+
+years_on_job = float(input("Enter years worked "))
+
+if salary >= 30000:
+
+    if years_on_job >= 2:
+
+        print("You get the loan")
+
+    else:
+
+        print("You do not meet the years on job requirements.")
+
+else:
+
+    print("You didn't meet salary requirements.)
